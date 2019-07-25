f2c803618f0f8a13eaef7ae8d30cf9c378d351ad
Abel lamesgen <abeltyr@gmail.com>
2019-07-25 14:55:01 +0300

    added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template lang="html">
 <div id="app">
-    <div class="inner" v-bind:style="{gridTemplateRows:back}">
-        <div v-for="row in rows" class="rows" />
+    <div class="inner" v-bind:style="{gridTemplateRows:rowNo}">
+        <div class="rows" v-for="(row,rowindex) in rows" v-bind:style="{gridTemplateColumns:columnNo}">
+            <div 
+              v-for="(column, columnindex) in columns"
+              v-bind:class="{ column: !true, columns: obe[rowindex+"."+ columnindex] }" >
+              {{}}
+              </div>
+        </div>
     </div>
     <div class="inner">
-        <button v-on:click="rows">call</button>
+       <div class="lower-rows" v-for="(row,rowindex) in rows" v-bind:style="{gridTemplateColumns:columnNo}">
+            <div 
+            class="lower-columns" 
+            v-for="(column, columnindex) in columns"
+        
+              v-on:click="clicked(rowindex, columnindex )"/>
+            </div>
+        </div>
     </div>
 </div>
 </template>
@@ -18,19 +31,36 @@ export default {
         HelloWorld
     },
     data: () => ({
-        back: "",
-        rows: [1, 2, 3],
-        column: [1, 2, 3, 4, 5]
+        rowNo: "",
+        look: "",
+        obe:{},
+        columnNo: "",
+        rows: [1, 2, 3,4, 5],
+        columns: [1, 2, 3,4, 5]
     }),
+    created() {
+        this.call();
+    },
     methods: {
-        rows(){
-          this.rows.forEach(row =>{
-            this.back = this.back + " 1fr"
-          })
+        clicked(rowIndex, columnIndex){
+          this.obe[rowIndex+"."+columnIndex] = false
+          console.log(this.obe)
         },
         call() {
-            alert("call")
-            $value = "1fr";
+            if (this.columnNo === "") {
+                this.columns.forEach(() => {
+                    this.columnNo = this.columnNo + " 1fr"
+                })
+            }
+            if (this.rowNo === "") {
+                this.rows.forEach((row, rowIndex) => {
+                    this.rowNo = this.rowNo + " 1fr"
+                    this.columns.forEach((column, columnIndex) => {
+                        this.obe[rowIndex+"."+columnIndex] = true
+                    })
+                })
+            }
+            console.log(this.obe)
         }
     }
 }
@@ -46,7 +76,9 @@ export default {
     height: 100vh;
     width: 100vw;
     display: grid;
+    background: #212121;
     grid-template-rows: 1fr 1fr;
+    grid-gap: 15px;
 }
 
 .inner {
@@ -54,10 +86,36 @@ export default {
     display: grid;
     height: 100%;
     width: 100%;
-    /* grid-template-rows: 1fr 1fr 1fr; */
+}
+.inner:first-child {
+    background: #212121;
+    display: grid;
+    height: 100%;
+    width: 100%;
+
 }
 
 .rows {
     background: #97217d;
+    display: grid;
+}
+.lower-rows {
+    background: #97217d;
+    display: grid;
+}
+
+.columns {
+    background: #219774;
+    border: 2px solid #cec7c7;
+}
+
+.column {
+    background-image: url("./assets/X.png");
+    border: 2px solid #cec7c7;
+    background-size: 100%;
+}
+.lower-columns{
+    background: #217f97;
+    border: 2px solid #cec7c7;
 }
 </style>
